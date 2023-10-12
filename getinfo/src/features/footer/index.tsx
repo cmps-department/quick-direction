7a0cb532db6653019fe518f79a8039d182759663
Yana <yanochkalaz0560@gmail.com>
2023-10-12 11:51:56 +0300

    footer

--- getinfo/src/features/footer/index.tsx
+++ getinfo/src/features/footer/index.tsx
@@ -1,7 +1,26 @@
-const Footer = () => {
+import { FC } from 'react'
+import styles from './footer.module.scss'
+import Image from 'next/image'
+
+import khpiImg from "../../assets/images/khpi-logo.svg"
+
+const Footer: FC = () => {
   return (
     <footer>
-      Footer
+      <div className={styles.footer}>
+        <ul>
+          <li>Контакти</li>
+          <li>Соціальні мережі</li>
+          <li>Про нас</li>
+        </ul>
+        <Image
+          width={192}
+          height={192}
+          src={khpiImg}
+          alt="Logo"
+        />
+        <div></div>
+      </div>
     </footer>
   )
 }
--- getinfo/src/features/homepage/index.tsx
+++ getinfo/src/features/homepage/index.tsx
@@ -1,8 +1,15 @@
 import { FC } from "react"
 import Layout from "../../layouts/Layout"
+import styles from './header.module.scss'
 
 const Home: FC = () => {
-    return <Layout title={"Home"} isLogin={true}>Home</Layout>
+    return (
+        <Layout title={"Home"} isLogin={true}>
+            <div className={styles.home}>
+                <h1>Home</h1>
+            </div>
+        </Layout>
+    )
 }
 
 export default Home
