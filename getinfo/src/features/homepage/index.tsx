c96437022750d3db3ceb51fad3218df97ac8f6a1
Yana <yanochkalaz0560@gmail.com>
2023-10-12 14:28:55 +0300

    last section

--- getinfo/src/features/homepage/index.tsx
+++ getinfo/src/features/homepage/index.tsx
@@ -2,12 +2,14 @@ import { FC } from "react"
 import Layout from "../../layouts/Layout"
 import AdvantagesSection from "./sections/advantages"
 import GuideSection from "./sections/guide"
+import HelpSection from "./sections/help"
 
 const Home: FC = () => {
     return (
         <Layout title={"Home"} isLogin={true}>
             <AdvantagesSection/>
             <GuideSection/>
+            <HelpSection/>
         </Layout>
     )
 }
---
+++ getinfo/src/features/homepage/sections/help/index.tsx
@@ -0,0 +1,35 @@
+import { FC } from "react"
+import styles from './help.module.scss'
+import CustomButton from "../../../../components/button/CustomButton"
+
+const HelpSection: FC = () => {
+    return (
+        <section>
+            <div className={styles.help}>
+                <div className={styles.helpCard}>
+                    <h3 className={styles.helpCard_title}>Є запитання?</h3>
+                    <p className={styles.helpCard_desc}>Ймовірно, що відповідь на ваше запитання уже є в FAQ.</p>
+                    <CustomButton className={styles.helpCard_btn} onClick={() => { }}>
+                        Перейти до FAQ
+                    </CustomButton>
+                </div>
+                <div className={styles.helpCard}>
+                    <h3 className={styles.helpCard_title}>Не знайшли відповідь?</h3>
+                    <p className={styles.helpCard_desc}>Наша технічна підтримка має відповіді на решту запитань.</p>
+                    <CustomButton className={styles.helpCard_btn} onClick={() => { }}>
+                        Задати питання
+                    </CustomButton>
+                </div>
+                <div className={styles.helpCard}>
+                    <h3 className={styles.helpCard_title}>Не маєте запитань?</h3>
+                    <p className={styles.helpCard_desc}>Подайте заявку на отримання довідки прямо зараз!</p>
+                    <CustomButton className={styles.helpCard_btn} onClick={() => { }}>
+                        Подати заявку
+                    </CustomButton>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default HelpSection
