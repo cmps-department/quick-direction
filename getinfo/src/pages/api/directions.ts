f50ebfc9f1b7b87e4d152e07a3429e9228555dda
BroodCaster <kilmichenkoanton228@gmail.com>
2023-10-19 15:04:30 +0300

    deleted .env file

--- getinfo/src/pages/api/directions.ts
+++ getinfo/src/pages/api/directions.ts
@@ -23,7 +23,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           console.error(error);
           res.status(500).json({ error: "An error occurred while creating the direction." });
         }
-      } else {
+      }else if(req.method === "GET"){
+        const allDirections = await prisma.directions.findMany()
+        if(allDirections){
+          res.status(201).json(allDirections)
+        }else{
+          res.status(500).json({errro: "There is no directions"})
+        }
+      }else {
         res.status(405).end(); // Method Not Allowed
       }
 }
