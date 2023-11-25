31dc34a34f312e86b70bc846eb2c3f679c3837b6
Chaplin Artem <75485190+ArCheeq@users.noreply.github.com>
2023-11-25 23:52:54 +0200

    GIK-41, GIK-42 added filters

--- src/features/RequestProcessingView/hooks/useRequests.ts
+++ src/features/RequestProcessingView/hooks/useRequests.ts
@@ -2,8 +2,10 @@ import { useMemo } from "react";
 import { useQuery } from "@tanstack/react-query";
 import api from "../../../utils/api";
 import { IRequest } from "../../../interfaces/request.interface";
+import { useSession } from "next-auth/react";
 
 export const useRequests = () => {
+    const { data: session } = useSession();
     const { data, ...options } = useQuery(
         ["REQUESTS"],
         () => api.get<IRequest[]>("/api/requests")
@@ -11,8 +13,20 @@ export const useRequests = () => {
 
     const requests = useMemo(() => {
         if (!data) return [];
-        return data.data;
+
+        if (session?.roles.includes("ROLE_ADMIN")) {
+            return data.data;
+        } else if (session?.roles.includes("ROLE_TEACHER")) {
+            return data.data.filter((request) => {
+                console.log(request.direction.professor, session.user.email)
+                return request.direction.professor === session.user.email
+            });
+        } else {
+            return data.data.filter((request) => request.userId === session?.user.userId);
+        }
     }, [data]);
+
+
 
     return {
         requests,
--- src/pages/api/direction/get/[id].ts
+++ src/pages/api/direction/get/[id].ts
@@ -5,29 +5,33 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    const id = Number(req.query.id);
+    if (req.method === 'GET') {
+        const id = Number(req.query.id);
 
-    if (isNaN(id)) {
-      res.status(400).json({ error: 'Invalid ID format' });
-      return;
-    }
+        if (isNaN(id)) {
+            res.status(400).json({ error: 'Invalid ID format' });
+        }
 
-    const oneDirection = await prisma.directions.findFirst({
-      where: {
-        id: id,
-      },
-      include: { 
-        subDirections: true
-      }
-    });
+        try {
+            const oneDirection = await prisma.directions.findFirst({
+                where: {
+                    id: id,
+                },
+                include: { 
+                    subDirections: true
+                }
+            });
 
-    if (oneDirection) {
-      res.status(201).json(oneDirection);
+            if (oneDirection) {
+                res.status(201).json(oneDirection);
+            } else {
+                res.status(500).json({ message: 'Category not found' });
+            }
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ error: "An error occurred while creating the direction." });
+        }
     } else {
-      res.status(500).json({ message: 'Category not found' });
+        res.status(405).json({ error: 'Method Not Allowed' });
     }
-  } else {
-    res.status(405).json({ error: 'Method Not Allowed' });
-  }
 }
