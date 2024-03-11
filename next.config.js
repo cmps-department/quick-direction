733726c9d4bb926b9432709b82ada080b417dac8
mborodin <m.borodin.1999@gmail.com>
2024-03-11 21:09:34 +0100

    added comments

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
+    // Збирає standalone білд. Такі білди не містять
+    // в собі повної копії node_modules і тому займають ЗНАЧНО (~в 10 разів) менше місця
     output: 'standalone',
     env: {
         API_URL: process.env.NEXT_BACKEND_URL,
