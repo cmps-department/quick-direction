0b8cd4e3f6e2f0982c304b92b97c4cf08b1a60ca
BroodCaster <kilmichenkoanton228@gmail.com>
2024-03-14 18:17:17 +0200

    added authorization to Requests GET endpoint

--- src/pages/api/requests/[id].ts
+++ src/pages/api/requests/[id].ts
@@ -1,25 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
+import { getServerSession } from "next-auth/next"
+import authOptions from "../auth/[...nextauth]"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === "GET") {
-        const id = Number(req.query.id);
+    const session = await getServerSession(req, res, authOptions)
+    if (session) {
+        if (req.method === "GET") {
+            const id = Number(req.query.id);
 
-        if (!id) {
-            res.status(400).json({ error: "No requestId provided" });
-        }
+            if (!id) {
+                res.status(400).json({ error: "No requestId provided" });
+            }
 
-        const requests = await prisma.request.findUnique({
-            where: { id },
-            include: { direction: true, subDirection: true },
-        });
+            const requests = await prisma.request.findUnique({
+                where: { id },
+                include: { direction: true, subDirection: true },
+            });
 
-        if (requests) {
-            res.status(201).json(requests);
+            if (requests) {
+                res.status(201).json(requests);
+            } else {
+                res.status(500).json({ error: "There is no request" });
+            }
         } else {
-            res.status(500).json({ error: "There is no request" });
+            res.status(405).end();
         }
     } else {
-        res.status(405).end();
+        res.status(405).json({error: "User is not authorized"})
     }
 }
