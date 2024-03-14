6bc60c870c4ab71e5be382369ef69b44a8978702
BroodCaster <kilmichenkoanton228@gmail.com>
2024-03-14 13:43:18 +0200

    added authorization and validation to Requests endpoint

--- src/pages/api/requests/index.ts
+++ src/pages/api/requests/index.ts
@@ -1,107 +1,133 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
+import { getServerSession } from "next-auth/next"
+import authOptions from "../auth/[...nextauth]"
+import Joi from "joi"
+
+const requestValidationSchema = Joi.object({
+    name: Joi.string().required(),
+    surname: Joi.string().required(),
+    email: Joi.string().email().required(),
+    studentGroup: Joi.string().required(),
+    userId: Joi.string().required(),
+    text: Joi.string().required(),
+    status: Joi.string().valid('Submitted', 'Processing', 'Clarify', 'Clarified', 'Processed', 'Canceled').required(),
+    documentLink: Joi.string().required(),
+    directionId: Joi.number().integer().required(),
+    subDirectionId: Joi.number().integer().required(),
+});
+
+const patchRequestValidationSchema = Joi.object({
+    id: Joi.number().integer().required(),
+    status: Joi.string().valid('Submitted', 'Processing', 'Clarify', 'Clarified', 'Processed', 'Canceled').required(),
+});
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === "POST") {
-        try {
-            const { name, surname, email, studentGroup, userId, text, status, directionId, subDirectionId, documentLink } = req.body;
+    const session = await getServerSession(req, res, authOptions)
+    if (session) {
+        if (req.method === "POST") {
+            try {
+                const { error, value } = requestValidationSchema.validate(req.body);
 
-            const newRequest = await prisma.request.create({
-                data: {
-                    name,
-                    surname,
-                    email,
-                    studentGroup,
-                    userId,
-                    text,
-                    status,
-                    documentLink,
-                    directionId,
-                    subDirectionId,
-                },
-            });
+                if (error) {
+                    return res.status(400).json({ error: error.details[0].message });
+                }
 
-            res.status(201).json(newRequest);
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: "An error occurred while creating the request." });
-        }
-    } else if (req.method === "GET") {
-        const id = Number(req.query.id);
+                const newRequest = await prisma.request.create({
+                    data: value
+                });
 
-        if (!id) {
-            const allRequests = await prisma.request.findMany({
-                include: {
-                    direction: true,
-                    subDirection: true,
-                    messages: true,
-                },
-            });
-
-            if (allRequests) {
-                res.status(201).json(allRequests);
-            } else {
-                res.status(500).json({ error: "There is no request" });
+                res.status(201).json(newRequest);
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ error: error });
             }
-        } else {
-            const requests = await prisma.request.findUnique({
+        } else if (req.method === "GET") {
+            const id = Number(req.query.id);
+
+            if (!id) {
+                const allRequests = await prisma.request.findMany({
+                    include: {
+                        direction: true,
+                        subDirection: true,
+                        messages: true,
+                    },
+                });
+
+                if (allRequests) {
+                    res.status(201).json(allRequests);
+                } else {
+                    res.status(500).json({ error: "There is no request" });
+                }
+            } else {
+                const requests = await prisma.request.findUnique({
+                    where: {
+                        id: id,
+                    },
+                });
+
+                if (requests) {
+                    res.status(201).json(requests);
+                } else {
+                    res.status(500).json({ error: "There is no request" });
+                }
+            }
+        } else if (req.method === "DELETE") {
+            const id = Number(req.query.id);
+
+            if (isNaN(id)) {
+                res.status(400).json({ error: "Invalid ID format" });
+                return;
+            }
+
+            const request = await prisma.request.delete({
                 where: {
                     id: id,
                 },
             });
 
-            if (requests) {
-                res.status(201).json(requests);
+            if (request) {
+                res.status(201).json({ message: "Succesfuly deleted" });
             } else {
-                res.status(500).json({ error: "There is no request" });
+                res.status(500).json({ message: "There is no directions" });
             }
-        }
-    } else if (req.method === "DELETE") {
-        const id = Number(req.query.id);
+        } else if (req.method === "PATCH") {
+            const { error, value } = patchRequestValidationSchema.validate(req.body);
 
-        if (isNaN(id)) {
-            res.status(400).json({ error: "Invalid ID format" });
-            return;
-        }
+            if (error) {
+                return res.status(400).json({ error: error.details[0].message });
+            }
 
-        const request = await prisma.request.delete({
-            where: {
-                id: id,
-            },
-        });
+            const { id, status } = value;
 
-        if (request) {
-            res.status(201).json({ message: "Succesfuly deleted" });
+            if (Array.isArray(id)) {
+                res.status(400).json({ error: "Multiple IDs are not supported" });
+                return;
+            }
+
+            if (isNaN(id)) {
+                res.status(400).json({ error: "Invalid ID format" });
+                return;
+            }
+            try {
+                const updatedRequest = await prisma.request.update({
+                    where: { id },
+                    data: { status },
+                });
+
+                if (updatedRequest) {
+                    res.status(201).json(updatedRequest);
+                } else {
+                    res.status(500).json({ error: "There is no directions" });
+                }
+            } catch (error) {
+                console.log(error);
+                res.status(500).json({ error: error });
+            }
         } else {
-            res.status(500).json({ message: "There is no directions" });
-        }
-    } else if (req.method === "PATCH") {
-        const { id, status } = req.body;
-
-        if (Array.isArray(id)) {
-            res.status(400).json({ error: "Multiple IDs are not supported" });
-            return;
-        }
-
-        if (isNaN(id)) {
-            res.status(400).json({ error: "Invalid ID format" });
-            return;
-        }
-        try {
-            const updatedRequest = await prisma.request.update({
-                where: { id },
-                data: { status },
-            });
-
-            if (updatedRequest) {
-                res.status(201).json(updatedRequest);
-            } else {
-                res.status(500).json({ error: "There is no directions" });
-            }
-        } catch (error) {
-            console.log(error);
+            res.status(405).end();
         }
     } else {
-        res.status(405).end();
+        res.status(405).json({error: "User is not authorized"})
     }
 }
