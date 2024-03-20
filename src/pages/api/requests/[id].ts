e9d9a72a5b09ffac4745d7be36e5f87329df61ca
BroodCaster <kilmichenkoanton228@gmail.com>
2024-03-20 14:55:39 +0200

    changed endpoint structure, added restriction to endpoints by user role

--- src/pages/api/requests/[id].ts
+++ src/pages/api/requests/[id].ts
@@ -1,10 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
 import { getServerSession } from "next-auth/next"
-import authOptions from "../auth/[...nextauth]"
+import { authConfig } from "@/configs/auth"
+import Joi from 'joi';
+import roles from "@/constants/roles";
+
+const patchRequestValidationSchema = Joi.object({
+    id: Joi.number().integer().required(),
+    status: Joi.string().valid('Submitted', 'Processing', 'Clarify', 'Clarified', 'Processed', 'Canceled').required(),
+});
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const session = await getServerSession(req, res, authOptions)
+    const session = await getServerSession(req, res, authConfig)
     if (session) {
         if (req.method === "GET") {
             const id = Number(req.query.id);
@@ -12,16 +19,115 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             if (!id) {
                 res.status(400).json({ error: "No requestId provided" });
             }
+            if (session?.roles.includes(roles.ROLE_SUPPORT_ADMIN)) {
+                const requests = await prisma.request.findUnique({
+                    where: { id },
+                    include: { direction: true, subDirection: true },
+                });
 
-            const requests = await prisma.request.findUnique({
-                where: { id },
-                include: { direction: true, subDirection: true },
-            });
+                if (requests) {
+                    res.status(201).json(requests);
+                } else {
+                    res.status(500).json({ error: "There is no request" });
+                }
+            } else if (session?.roles.includes(roles.ROLE_STUDENT)) {
+                const requests = await prisma.request.findUnique({
+                    where: { id },
+                    include: { direction: true, subDirection: true },
+                });
 
-            if (requests) {
-                res.status(201).json(requests);
-            } else {
-                res.status(500).json({ error: "There is no request" });
+                if (requests?.userId === session.user.userId) {
+                    res.status(201).json(requests);
+                } else {
+                    res.status(500).json({ error: "There is no request" });
+                }
+            } else if (session?.roles.includes(roles.ROLE_TEACHER)) {
+                const requests = await prisma.request.findUnique({
+                    where: { id },
+                    include: { direction: true, subDirection: true },
+                });
+
+                if (requests?.direction.professor === session.user.email) {
+                    res.status(201).json(requests);
+                } else {
+                    res.status(500).json({ error: "There is no request" });
+                }
+            }
+        } else if (req.method === "PATCH") {
+            const { error, value } = patchRequestValidationSchema.validate(req.body);
+
+            if (error) {
+                return res.status(400).json({ error: error.details[0].message });
+            }
+
+            const { id, status } = value;
+
+            if (Array.isArray(id)) {
+                res.status(400).json({ error: "Multiple IDs are not supported" });
+                return;
+            }
+
+            if (isNaN(id)) {
+                res.status(400).json({ error: "Invalid ID format" });
+                return;
+            }
+            try {
+                if (session?.roles.includes(roles.ROLE_SUPPORT_ADMIN)) {
+                    const updatedRequest = await prisma.request.update({
+                        where: { id },
+                        data: { status },
+                    });
+
+                    if (updatedRequest) {
+                        res.status(201).json(updatedRequest);
+                    } else {
+                        res.status(500).json({ error: "There is no request" });
+                    }
+                } else if (session?.roles.includes(roles.ROLE_STUDENT)) {
+                    const requests = await prisma.request.findUnique({
+                        where: { id },
+                        include: { direction: true, subDirection: true },
+                    });
+
+                    if (requests?.userId === session.user.userId) {
+                        const updatedRequest = await prisma.request.update({
+                            where: { id },
+                            data: { status },
+                        });
+
+                        if (updatedRequest) {
+                            res.status(201).json(updatedRequest);
+                        } else {
+                            res.status(500).json({ error: "There is no request" });
+                        }
+                    } else {
+                        res.status(405).json({ error: "You do not have permission to perform this operation" });
+                    }
+                } else if (session?.roles.includes(roles.ROLE_TEACHER)) {
+                    const requests = await prisma.request.findUnique({
+                        where: { id },
+                        include: { direction: true, subDirection: true },
+                    });
+
+                    if (requests?.direction.professor === session.user.email) {
+                        const updatedRequest = await prisma.request.update({
+                            where: { id },
+                            data: { status },
+                        });
+
+                        if (updatedRequest) {
+                            res.status(201).json(updatedRequest);
+                        } else {
+                            res.status(500).json({ error: "There is no request" });
+                        }
+                    } else {
+                        res.status(405).json({ error: "You do not have permission to perform this operation" });
+                    }
+                }
+
+            } catch (error) {
+                console.log(error);
+                res.status(500).json({ error: error });
             }
         } else {
             res.status(405).end();
--- src/pages/api/requests/index.ts
+++ src/pages/api/requests/index.ts
@@ -1,8 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
 import { getServerSession } from "next-auth/next"
-import authOptions from "../auth/[...nextauth]"
+import { authConfig } from "@/configs/auth"
 import Joi from "joi"
+import roles from "@/constants/roles";
 
 const requestValidationSchema = Joi.object({
     name: Joi.string().required(),
@@ -17,35 +18,32 @@ const requestValidationSchema = Joi.object({
     subDirectionId: Joi.number().integer().required(),
 });
 
-const patchRequestValidationSchema = Joi.object({
-    id: Joi.number().integer().required(),
-    status: Joi.string().valid('Submitted', 'Processing', 'Clarify', 'Clarified', 'Processed', 'Canceled').required(),
-});
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const session = await getServerSession(req, res, authOptions)
+    const session = await getServerSession(req, res, authConfig)
     if (session) {
         if (req.method === "POST") {
-            try {
-                const { error, value } = requestValidationSchema.validate(req.body);
+            if (session?.roles.includes(roles.ROLE_STUDENT)) {
+                try {
+                    const { error, value } = requestValidationSchema.validate(req.body);
 
-                if (error) {
-                    return res.status(400).json({ error: error.details[0].message });
+                    if (error) {
+                        return res.status(400).json({ error: error.details[0].message });
+                    }
+
+                    const newRequest = await prisma.request.create({
+                        data: value
+                    });
+
+                    res.status(201).json(newRequest);
+                } catch (error) {
+                    console.error(error);
+                    res.status(500).json({ error: error });
                 }
-
-                const newRequest = await prisma.request.create({
-                    data: value
-                });
-
-                res.status(201).json(newRequest);
-            } catch (error) {
-                console.error(error);
-                res.status(500).json({ error: error });
+            } else {
+                res.status(405).json({ error: "Only students can create request" });
             }
         } else if (req.method === "GET") {
-            const id = Number(req.query.id);
-
-            if (!id) {
+            if (session?.roles.includes(roles.ROLE_SUPPORT_ADMIN)) {
                 const allRequests = await prisma.request.findMany({
                     include: {
                         direction: true,
@@ -59,70 +57,42 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 } else {
                     res.status(500).json({ error: "There is no request" });
                 }
-            } else {
-                const requests = await prisma.request.findUnique({
+            } else if (session?.roles.includes(roles.ROLE_STUDENT)) {
+                const allRequests = await prisma.request.findMany({
                     where: {
-                        id: id,
+                      userId: session?.user.userId
+                    },
+                    include: {
+                        direction: true,
+                        subDirection: true,
+                        messages: true,
                     },
                 });
 
-                if (requests) {
-                    res.status(201).json(requests);
+                if (allRequests) {
+                    res.status(201).json(allRequests);
                 } else {
                     res.status(500).json({ error: "There is no request" });
                 }
-            }
-        } else if (req.method === "DELETE") {
-            const id = Number(req.query.id);
-
-            if (isNaN(id)) {
-                res.status(400).json({ error: "Invalid ID format" });
-                return;
-            }
-
-            const request = await prisma.request.delete({
-                where: {
-                    id: id,
-                },
-            });
-
-            if (request) {
-                res.status(201).json({ message: "Succesfuly deleted" });
-            } else {
-                res.status(500).json({ message: "There is no directions" });
-            }
-        } else if (req.method === "PATCH") {
-            const { error, value } = patchRequestValidationSchema.validate(req.body);
-
-            if (error) {
-                return res.status(400).json({ error: error.details[0].message });
-            }
-
-            const { id, status } = value;
-
-            if (Array.isArray(id)) {
-                res.status(400).json({ error: "Multiple IDs are not supported" });
-                return;
-            }
-
-            if (isNaN(id)) {
-                res.status(400).json({ error: "Invalid ID format" });
-                return;
-            }
-            try {
-                const updatedRequest = await prisma.request.update({
-                    where: { id },
-                    data: { status },
+            } else if (session?.roles.includes(roles.ROLE_TEACHER)) {
+                const allRequests = await prisma.request.findMany({
+                    where: {
+                      direction: {
+                          professor: session.user.email,
+                      },
+                    },
+                    include: {
+                        direction: true,
+                        subDirection: true,
+                        messages: true,
+                    },
                 });
 
-                if (updatedRequest) {
-                    res.status(201).json(updatedRequest);
+                if (allRequests) {
+                    res.status(201).json(allRequests);
                 } else {
-                    res.status(500).json({ error: "There is no directions" });
+                    res.status(500).json({ error: "There is no request" });
                 }
-            } catch (error) {
-                console.log(error);
-                res.status(500).json({ error: error });
             }
         } else {
             res.status(405).end();
